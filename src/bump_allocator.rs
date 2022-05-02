c581a516b6933375f48d71725d329bdcab632db9
k-nasa <htilcs1115@gmail.com>
2022-05-02 13:00:50 +0900

    Add simple allocator

--- src/bump_allocator.rs
+++ src/bump_allocator.rs
@@ -5,6 +5,7 @@ use std::{
 };
 
 pub const MEM_SIZE: usize = 64 * 1024;
+
 pub struct BumpAllocator {
     pub(crate) mem: UnsafeCell<[u8; MEM_SIZE]>,
     pub(crate) next: AtomicUsize,
@@ -15,11 +16,23 @@ unsafe impl Sync for BumpAllocator {}
 
 unsafe impl GlobalAlloc for BumpAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let next = self.next.load(Ordering::SeqCst);
-
         self.allocations.fetch_add(1, Ordering::SeqCst);
-        self.next.fetch_add(layout.size(), Ordering::SeqCst);
+        self.next
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |mut next| {
+                let align = layout.align();
+                let size = layout.size();
 
+                let size = if size % align != 0 {
+                    ((size / align) + 1) * align
+                } else {
+                    size
+                };
+                next += size + 1;
+                Some(next)
+            })
+            .unwrap();
+
+        let next = self.next.load(Ordering::SeqCst);
         (self.mem.get() as *mut u8).add(next)
     }
 
