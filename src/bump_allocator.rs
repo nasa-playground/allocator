780fe248915697d4769e5c8e06ea21be86385c80
k-nasa <htilcs1115@gmail.com>
2022-05-02 13:57:41 +0900

    Add test

--- src/bump_allocator.rs
+++ src/bump_allocator.rs
@@ -4,7 +4,7 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-pub const MEM_SIZE: usize = 64 * 1024;
+pub const MEM_SIZE: usize = 6400 * 1024;
 
 pub struct BumpAllocator {
     pub(crate) mem: UnsafeCell<[u8; MEM_SIZE]>,
@@ -18,17 +18,12 @@ unsafe impl GlobalAlloc for BumpAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         self.allocations.fetch_add(1, Ordering::SeqCst);
         self.next
-            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |mut next| {
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |next| {
                 let align = layout.align();
                 let size = layout.size();
 
-                let size = if size % align != 0 {
-                    ((size / align) + 1) * align
-                } else {
-                    size
-                };
-                next += size + 1;
-                Some(next)
+                let size = calc_size(size, align);
+                Some(next + size)
             })
             .unwrap();
 
@@ -38,5 +33,25 @@ unsafe impl GlobalAlloc for BumpAllocator {
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
         // do not something
+    }
+}
+
+fn calc_size(size: usize, align: usize) -> usize {
+    let size = if size % align == 0 {
+        size
+    } else {
+        ((size / align) + 1) * align
+    };
+
+    size
+}
+
+#[test]
+fn calc_size_test() {
+    let test_cases = vec![(1, 8, 8), (2, 8, 8), (8, 8, 8), (9, 8, 16), (24, 8, 24)];
+
+    for test_case in test_cases {
+        let actual = calc_size(test_case.0, test_case.1);
+        assert_eq!(actual, test_case.2)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,20 @@ use std::{cell::UnsafeCell, sync::atomic::AtomicUsize};
 // };
 
 // simple allocator
-#[global_allocator]
-static ALLOCATOR: simple_allocator::SimpleAllocator = simple_allocator::SimpleAllocator {
-    arena: UnsafeCell::new([0x55; simple_allocator::ARENA_SIZE]),
-    remaining: AtomicUsize::new(simple_allocator::ARENA_SIZE),
-};
+// #[global_allocator]
+// static ALLOCATOR: simple_allocator::SimpleAllocator = simple_allocator::SimpleAllocator {
+//     arena: UnsafeCell::new([0x55; simple_allocator::ARENA_SIZE]),
+//     remaining: AtomicUsize::new(simple_allocator::ARENA_SIZE),
+// };
 
 fn main() {
     let s1 = format!("allocating a string1");
     let s2 = format!("allocating a string2");
-    let _b = Box::new(1);
-    let _v: Vec<usize> = vec![1, 2, 3];
 
     println!("{s1}");
     println!("{s2}");
+    let _b = Box::new(1);
+    let _v: Vec<usize> = vec![1, 2, 3];
 
     // unsafe {
     //     let mem = ALLOCATOR.mem.get();
