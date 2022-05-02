6b11905221e37d1f17da456b60e1e591f3610ca6
k-nasa <htilcs1115@gmail.com>
2022-05-02 14:52:56 +0900

    update

--- src/bump_allocator.rs
+++ src/bump_allocator.rs
@@ -4,31 +4,30 @@ use std::{
     sync::atomic::{AtomicUsize, Ordering},
 };
 
-pub const MEM_SIZE: usize = 6400 * 1024;
+pub const MEM_SIZE: usize = 128 * 1024;
 
 pub struct BumpAllocator {
     pub(crate) mem: UnsafeCell<[u8; MEM_SIZE]>,
     pub(crate) next: AtomicUsize,
-    pub(crate) allocations: AtomicUsize,
 }
 
 unsafe impl Sync for BumpAllocator {}
 
 unsafe impl GlobalAlloc for BumpAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        self.allocations.fetch_add(1, Ordering::SeqCst);
-        self.next
-            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |next| {
-                let align = layout.align();
-                let size = layout.size();
+        let next = self.next.load(Ordering::SeqCst);
+        let res = (self.mem.get() as *mut u8).add(next);
 
-                let size = calc_size(size, align);
-                Some(next + size)
+        let align = layout.align();
+        let size = layout.size();
+        self.next
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |mut next| {
+                next += size;
+                Some(alignment_correction(next, align))
             })
             .unwrap();
 
-        let next = self.next.load(Ordering::SeqCst);
-        (self.mem.get() as *mut u8).add(next)
+        res
     }
 
     unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
@@ -36,22 +35,16 @@ unsafe impl GlobalAlloc for BumpAllocator {
     }
 }
 
-fn calc_size(size: usize, align: usize) -> usize {
-    let size = if size % align == 0 {
-        size
-    } else {
-        ((size / align) + 1) * align
-    };
-
-    size
+fn alignment_correction(size: usize, align: usize) -> usize {
+    (size + align - 1) & !(align - 1)
 }
 
 #[test]
-fn calc_size_test() {
+fn alignment_correction_test() {
     let test_cases = vec![(1, 8, 8), (2, 8, 8), (8, 8, 8), (9, 8, 16), (24, 8, 24)];
 
     for test_case in test_cases {
-        let actual = calc_size(test_case.0, test_case.1);
+        let actual = alignment_correction(test_case.0, test_case.1);
         assert_eq!(actual, test_case.2)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,4 @@
 mod bump_allocator;
 mod simple_allocator;
 
-use std::{cell::UnsafeCell, sync::atomic::AtomicUsize};
-
-// bump allocator
-// #[global_allocator]
-// static ALLOCATOR: bump_allocator::BumpAllocator = bump_allocator::BumpAllocator {
-//     mem: UnsafeCell::new([0u8; bump_allocator::MEM_SIZE]),
-//     next: AtomicUsize::new(0),
-//     allocations: AtomicUsize::new(0),
-// };
-
-// simple allocator
-// #[global_allocator]
-// static ALLOCATOR: simple_allocator::SimpleAllocator = simple_allocator::SimpleAllocator {
-//     arena: UnsafeCell::new([0x55; simple_allocator::ARENA_SIZE]),
-//     remaining: AtomicUsize::new(simple_allocator::ARENA_SIZE),
-// };
-
-fn main() {
-    let s1 = format!("allocating a string1");
-    let s2 = format!("allocating a string2");
-
-    println!("{s1}");
-    println!("{s2}");
-    let _b = Box::new(1);
-    let _v: Vec<usize> = vec![1, 2, 3];
-
-    // unsafe {
-    //     let mem = ALLOCATOR.mem.get();
-    //     println!("{:?}", mem.as_ref());
-    // }
-}
+fn main() {}
